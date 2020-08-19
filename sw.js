f315a86d934ebe0813dcf13e300a2e8db87e74b8
muyuu <anticyborg@gmail.com>
2020-08-19 10:29:24 +0900

    test.json のみ redirect.json にリダイレクトする

--- sw.js
+++ sw.js
@@ -8,7 +8,13 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
 	console.log('WORKER: Fetching', event.request);
+
+	const url = event.request.url;
+	const isTarget = url.includes('test.json');
+	const targetUrl = url.replace('test.json', 'redirect.json');
+	const target = isTarget ? targetUrl : url;
+
 	event.respondWith(
-		fetch(event.request)
+		fetch(target)
 	);
 });
